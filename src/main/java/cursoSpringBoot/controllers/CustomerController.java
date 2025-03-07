b7bcb816c9ae430a250d2bca2c27f45c88581a5e
asrodol <asrodol@gmail.com>
2025-03-06 23:25:14 -0600

    Se agrega clase Controller y Customer

---
+++ src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -0,0 +1,43 @@
+package cursoSpringBoot.controllers;
+
+import cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class CustomerController {
+
+    List<Customer> listCust = new ArrayList<>(Arrays.asList(
+            new Customer(1234, "Rodolfo", "asrodol", "0000"),
+            new Customer(1235, "Alejandro", "aslex", "1111"),
+            new Customer(1236, "Guillermo", "memo", "2222"),
+            new Customer(1237, "Israel", "pascual", "3333")
+    ));
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> showCustomers(){
+        return listCust;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Customer serarchCustomer(@PathVariable int id){
+        for (Customer c:listCust){
+            if (c.getId()==id){
+                 return c;
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Customer saveCustomer(@RequestBody Customer cus){
+        listCust.add(cus);
+
+        return  cus;
+    }
+
+}
---
+++ src/main/java/cursoSpringBoot/domain/Customer.java
@@ -0,0 +1,47 @@
+package cursoSpringBoot.domain;
+
+
+public class Customer {
+
+    private int id;
+    private String name, usaerName, password;
+
+    public Customer(int id, String name, String usaerName, String password) {
+        this.id = id;
+        this.name = name;
+        this.usaerName = usaerName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsaerName() {
+        return usaerName;
+    }
+
+    public void setUsaerName(String usaerName) {
+        this.usaerName = usaerName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
